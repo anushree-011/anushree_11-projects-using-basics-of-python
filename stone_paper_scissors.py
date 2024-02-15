1a77e42410073c20480d8513c24a9f8c8b6265a2
anushree-011 <115527877+anushree-011@users.noreply.github.com>
2024-02-15 18:41:16 +0530

    Update stone_paper_scissors.py
    used loop to upgrade this game

--- stone_paper_scissors.py
+++ stone_paper_scissors.py
@@ -1,5 +1,6 @@
 import random
-
+you=0
+computer=0
 rock = '''
     _______
 ---'   ____)
@@ -28,24 +29,40 @@ scissors = '''
 '''
 
 game_images = [rock, paper, scissors]
+1
+for i in range(10):
+  user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+  if user_choice >= 3 or user_choice < 0: 
+      print("You typed an invalid number, you lose!") 
+  else:
+      print(game_images[user_choice])
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game_images[user_choice])
+      computer_choice = random.randint(0, 2)
+      print("Computer chose:")
+      print(game_images[computer_choice])
+   
 
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(game_images[computer_choice])
+      if user_choice == 0 and computer_choice == 2:
+          print("You win!")
+          you+=1
+      elif computer_choice == 0 and user_choice == 2:
+          print("You lose")
+          computer+=1
+      elif computer_choice > user_choice:
+          print("You lose")
+          computer+=1
+      elif user_choice > computer_choice:
+          print("You win!")
+          you+=1
+      elif computer_choice == user_choice:
+          print("It's a draw")
+print(f"Your score:{you}")
+print(f"Computer score:{computer}")
+if you>computer:
+    print("YOUR THE WINNER!!")
+elif computer>you:
+    print("COMPUTER IS THE WINNER")
+else:
+    print("DRAW MATCH")
 
-if user_choice >= 3 or user_choice < 0: 
-  print("You typed an invalid number, you lose!") 
-elif user_choice == 0 and computer_choice == 2:
-  print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-  print("You lose")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice > computer_choice:
-  print("You win!")
-elif computer_choice == user_choice:
-  print("It's a draw")
 

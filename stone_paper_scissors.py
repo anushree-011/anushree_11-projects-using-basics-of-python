135e76c10ac372586b905809ebec1b363a0fc1ef
anushree-011 <115527877+anushree-011@users.noreply.github.com>
2024-02-15 20:53:51 +0530

    Update stone_paper_scissors.py

--- stone_paper_scissors.py
+++ stone_paper_scissors.py
@@ -30,7 +30,7 @@ scissors = '''
 
 game_images = [rock, paper, scissors]
 1
-for i in range(20):
+for i in range(15):
   user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
   if user_choice >= 3 or user_choice < 0: 
       print("You typed an invalid number, you lose!") 

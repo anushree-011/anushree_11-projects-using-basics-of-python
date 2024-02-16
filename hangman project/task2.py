bfd4b7b8329fb9aa023086f42942c1ed7638864c
anushree-011 <115527877+anushree-011@users.noreply.github.com>
2024-02-16 22:33:51 +0530

    Update task2.py

--- hangman project/task2.py
+++ hangman project/task2.py
@@ -5,7 +5,7 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+#print(f'Pssst, the solution is {chosen_word}.')
 
 #1: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.

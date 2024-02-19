2c87bd4c0ae9a2799d9f93b0778afd324b712872
anushree-011 <115527877+anushree-011@users.noreply.github.com>
2024-02-19 23:02:58 +0530

    Update passward_generator.py

--- passward_generator.py
+++ passward_generator.py
@@ -8,7 +8,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level
+#Eazy Level- int this the generated password will be in sequence like letters, symbols and then numbers
 password = ""
 
 for char in range(1, nr_letters + 1):

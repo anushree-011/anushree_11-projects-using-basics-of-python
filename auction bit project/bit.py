0f96118ced5e69aaf1c4a233dae2b527e42b5e82
anushree-011 <115527877+anushree-011@users.noreply.github.com>
2024-02-21 23:10:25 +0530

    Update bit.py

--- auction bit project/bit.py
+++ auction bit project/bit.py
@@ -2,24 +2,26 @@ from art import logo
 from replit import clear
 auction_bid={}
 go=1
-
+def auction(auction_bid):
+    winner=""
+    max=0
+    for n in auction_bid:
+       if auction_bid[n]>max:
+           max=auction_bid[n]
+           winner=n
+    print(f"the winner is {winner} with a bid of Rs{max}")
+    
 while(go):
     print(logo)
-    name=input("Enter your name:")
-    bit=int(input("Enter your bid:Rs"))
+    name=input("Enter your name: ")
+    bid=int(input("Enter your bid: Rs"))
     auction_bid[name]=bid
-    con=input("type 'yes to continue . type 'no' to end :")
+    con=input("type 'yes to continue . type 'no' to end : ")
     if con=="no":
         go=False
+        auction(auction_bid)
         print("Good bye")
     else:
         clear()
-max=0
-print(auction_bid)
-for n in auction_bid:
-    if auction_bid[n]>max:
-        max=auction_bid[n]
-for n in auction_bid:
-    if auction_bid[n]==max:
-        print(f"the winner is {n} with the bid of {auction_bid[n]}")
-        print("the auction is over, Thanks for joining")
+
+

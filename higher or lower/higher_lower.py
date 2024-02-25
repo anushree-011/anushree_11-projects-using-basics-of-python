e198af11a8ede06c2f34c0b5ea1983cf977fe20a
anushree-011 <115527877+anushree-011@users.noreply.github.com>
2024-02-25 20:25:21 +0530

    Update higher_lower.py

--- higher or lower/higher_lower.py
+++ higher or lower/higher_lower.py
@@ -1,26 +1,34 @@
 import random
 from data import data
-
-a=random.choice(data)
-b=random.choice(data)
-print(a)
-print(f"team A = {a['name']}, is a {a['description']} , from {a['country']}")
-print(b)
-print(f"team B = {b['name']}, is a {b['description']} , from {b['country']}")
+from replit import clear
 
 def compare(a,b):
     if a['follower_count']>b['follower_count']:
-        return True
+        return "a"
+    elif a['follower_count']<b['follower_count']:
+        return "b"
     else:
-        return False
+        return "b"
 
-def game(a,b):
-    guess=input("who do u think has higher followers on instagram : ")
-    if guess=="a" or guess=="A":
-        if compare(a,b):
+def game():
+    flag=True
+    score=0
+    while flag:
+        a=random.choice(data)
+        b=random.choice(data)
+        print(f"team A = {a['name']}, is a {a['description']} , from {a['country']}")
+        print(f"team B = {b['name']}, is a {b['description']} , from {b['country']}")
+        guess=input("who do u think has higher followers on instagram : ")
+        choice=compare(a,b)
+  
+        if guess==choice:
             print("your right")
+            score+=1
+            clear()
         else:
             print("your wrong")
+            print(f"final score= {score}")
+            flag=False
 
-game(a,b)
+game()
 
